3c0e079e4270497cc3844f7ba4b4d11c9fbba826
vbuxx <andhikapramana101@gmail.com>
2022-06-22 17:58:17 +0700

    Modify App.js & index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="todo-app">
+      <TodoList />
     </div>
   );
 }
