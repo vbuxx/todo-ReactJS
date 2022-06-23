0c7f8b58a09d79f78dd0147ec25196f534e25cb1
vbuxx <andhikapramana101@gmail.com>
2022-06-23 08:08:07 +0700

    Modify Heading

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -62,7 +62,7 @@ function TodoList() {
 
   return (
     <div>
-      <h1>Apa rencanamu?</h1>
+      <h1>ToDo Apps</h1>
       <TodoForm onSubmit={addTodo} />
       <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}
       />
